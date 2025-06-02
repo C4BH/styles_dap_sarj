9d841981ed9bf0a7adc1eec98848ba6b75eff3cb
C4BH <tcince44@gmail.com>
2025-06-02 17:07:46 +0300

    buttonlar eklendi

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -28,3 +28,81 @@ class ButtonStyles {
     padding: EdgeInsets.all(ProjectPaddings.paddingLarge),
   );
 }
+
+class MyElevatedButton extends ElevatedButton {
+  MyElevatedButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.style,
+  }) : super(style: style, child: Text(text), onPressed: onPressed);
+  final String text;
+  final VoidCallback onPressed;
+  final ButtonStyle style;
+}
+
+class MyTextButton extends TextButton {
+  final TextAlign textAlign;
+  final String text;
+  final VoidCallback onPressed;
+  final TextStyle textStyle;
+  MyTextButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.textStyle,
+    required this.textAlign,
+  }) : super(
+         child: Text(text, style: textStyle, textAlign: textAlign),
+         onPressed: onPressed,
+       );
+}
+
+class SocialButtons extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final String icon;
+  final double width;
+  final double height;
+  const SocialButtons({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    required this.icon,
+    required this.width,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Padding(
+        padding: EdgeInsets.all(ProjectPaddings.paddingSmall),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.43,
+          decoration: BoxDecoration(
+            border: Border.all(color: ProjectColors.primaryColor),
+            borderRadius: BorderRadius.all(
+              Radius.circular(ProjectBorderRadius.borderRadiusLarge),
+            ),
+            color: ProjectColors.secondaryColor,
+          ),
+          child: Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(ProjectPaddings.paddingSmall),
+                child: Image.asset(icon, width: width, height: height),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: ProjectPaddings.paddingSmall),
+                child: Text(text, style: ProjectTextStyle.subtitle),
+              ),
+              SizedBox(width: ProjectPaddings.paddingSmall),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
